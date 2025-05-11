c5b22f8922eb97f5aa656d1423a9b557a5d75f04
Mutiara Intania <mutiara.intania97@gmail.com>
2025-05-11 21:33:27 +0700

    add new keyword for explore case

---
+++ cypress/e2e/keywords/keywordExplore.js
@@ -0,0 +1,32 @@
+// this page only for save all keywords
+
+/// <reference types="cypress" />
+
+import { ExplorePages } from "../pages/explorePages"
+
+const explorePages = new ExplorePages()
+
+beforeEach(function(){
+    cy.visit('https://job-portal-user-dev-skx7zw44dq-et.a.run.app/mentoring')
+})
+
+export class KeywordExplore{
+
+    goToExploreTab(){
+        cy.wait(2000);
+        cy.contains(explorePages.exploreTab).should('be.exist');
+        cy.contains(explorePages.exploreTab).click();
+    }
+
+    searchOnExploreTab(keyword){
+        cy.wait(2000);
+        cy.get(explorePages.searchMentorBar).should('be.visible');
+        cy.get(explorePages.searchMentorBar).type(keyword);
+    }
+
+    verifySearchResults(){
+        cy.wait(2000);
+        cy.contains('Backend Tester').should('be.visible');
+    }
+
+}
